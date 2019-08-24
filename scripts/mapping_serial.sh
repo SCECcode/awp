da6b42165bc8eef6855829bff397330b13e1b549
Ossian O'Reilly <ooreilly@usc.edu>
2019-08-24 15:26:56 -0400

    Fix mapping script.

--- scripts/mapping_serial.sh
+++ scripts/mapping_serial.sh
@@ -2,7 +2,6 @@
 build=$1
 analysis=$2
 dir=logs/mapping
-submit=analysis
 nz=256
 
 if [ $build == 1 ]
@@ -27,11 +26,6 @@ mkdir -p ${dir}
 
 echo "#!/bin/bash" > $dir/${submit}.sh
 
-for nx in 100 200 300 400 500 600
-do
-        for ny in 100 200 300 400 500 600
-        do
-                filename=${submit}_${nx}_${ny}.lsf
 echo "#!/bin/bash
 # Begin LSF Directives
 #BSUB -P geo112
@@ -45,11 +39,14 @@ echo "#!/bin/bash
 module load cuda
 
 #cd $LS_SUBCWD
-bash scripts/run.sh ${dir} \"${nvflags}\" ${nx} ${ny} ${nz}
 " >    \
-$dir/${filename}_${nx}_${ny}.lsf
+$dir/${submit}.lsf
 
-echo "bsub $dir/${submit}_${nx}_${ny}.lsf
-" >> $dir/${submit}.sh
+for nx in 100 200 300 400 500 600
+do
+        for ny in 100 200 300 400 500 600
+        do
+echo "bash scripts/run.sh ${dir} \"${nvflags}\" ${nx} ${ny} ${nz}" >> \
+$dir/${submit}.lsf
         done
 done
